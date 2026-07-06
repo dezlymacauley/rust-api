aaa6af956f79b28e8956b775cc55f2e72e6b7b11
dezlymacauley <dezlymacauley@proton.me>
2026-07-06 15:16:38 +0200

    Get request is functional

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("\nRust API\n");
+use tokio::net::TcpListener;
+use axum::{
+    Router,
+    routing::get,
+    serve::serve
+};
+
+async fn root() -> &'static str {
+    println!("Endpoint hit: /");
+    "Welcome to the Rust API"
+}
+
+#[tokio::main]
+async fn main() {
+
+   let axum_router = Router::new().route("/", get(root));
+
+    let network_address: &str = "127.0.0.1:7878";
+
+    let tcp_listener = TcpListener::bind(network_address)
+        .await
+        .expect("TCP listener failed to connect");
+
+    println!("Listening on:");
+    println!("http://{network_address}");
+
+    println!();
+
+    serve(tcp_listener, axum_router)
+        .await
+        .expect("Axum failed failed to connect");
 }
